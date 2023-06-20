81ae4a69adc42457507b42d2e619e47bec33ada1
Gerald Bruno <34822464+NephilimEyez@users.noreply.github.com>
2023-06-20 17:36:02 -0400

    Working backend

--- server.js
+++ server.js
@@ -3,11 +3,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Books = require('./models/books');
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.get('/test', (request, response) => {
 
@@ -16,3 +18,31 @@ app.get('/test', (request, response) => {
 })
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+app.get('/books', getBooks);
+
+async function getBooks(request, response, next) {
+  try {
+    let allBooks = await Books.find({});
+    response.status(200).send(allBooks);
+  } catch (error) {
+    next(error)
+  }
+}
+
+app.get('*', (request, response) => {
+  response.status(404).send('Not available');
+});
+
+app.use((error, request, response, next) => {
+  console.log(error.message);
+  response.status(500).send(error.message);
+});
